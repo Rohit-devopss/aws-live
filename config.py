7a0c41a0c88d100879088ba89e40dcdd0c460a4a
Rohit-devopss <77105103+Rohit-devopss@users.noreply.github.com>
2021-08-29 11:06:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employiii.ciivd6aauqco.us-east-1.rds.amazonaws.com"
 customuser = "rohitshinde"
 custompass = "rohit5521"
-customdb = "employiii"
+customdb = "employee"
 custombucket = "maybes3"
 customregion = "us-east-1"
