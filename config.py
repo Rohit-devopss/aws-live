3ee8d1d5621746f72acc1f762c7ccda9b37d8775
Rohit-devopss <77105103+Rohit-devopss@users.noreply.github.com>
2022-03-11 19:09:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "organization.crthinyxhzst.us-east-1.rds.amazonaws.com"
 customuser = "admindb"
 custompass = "Rohit5521"
-customdb = "Organization"
+customdb = "organization"
 custombucket = "mark-2buket"
 customregion = "us-east-1"
